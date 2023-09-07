d72be5779d30980673ea6bed9594c3715a1d2c33
Clayder <peterclayder@gmail.com>
2023-09-07 19:07:28 -0300

    init

---
+++ src/main/kotlin/com/clayder/learn/hexagonalarch/HexagonalarchApplication.kt
@@ -0,0 +1,11 @@
+package com.clayder.learn.hexagonalarch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalarchApplication
+
+fun main(args: Array<String>) {
+	runApplication<HexagonalarchApplication>(*args)
+}
---
+++ src/test/kotlin/com/clayder/learn/hexagonalarch/HexagonalarchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clayder.learn.hexagonalarch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexagonalarchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
